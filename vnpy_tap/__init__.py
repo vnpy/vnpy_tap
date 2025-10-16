537a615299697ad026337eb60ae2373c2b9665d7
noranhe <63988546+noranhe@users.noreply.github.com>
2025-10-16 09:41:46 +0800

    [Mod] 调整模块的版本获取方式

--- vnpy_tap/__init__.py
+++ vnpy_tap/__init__.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import importlib_metadata
+from importlib import metadata
 
 from .gateway import TapGateway
 
@@ -29,6 +29,6 @@ __all__ = ["TapGateway"]
 
 
 try:
-    __version__ = importlib_metadata.version("vnpy_tap")
-except importlib_metadata.PackageNotFoundError:
+    __version__ = metadata.version("vnpy_tap")
+except metadata.PackageNotFoundError:
     __version__ = "dev"
